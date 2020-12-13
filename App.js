57cb568b89eeef2bb05cdcf3fc04979d118d87be
mayura1996 <mmayurapavan@gmail.com>
2020-12-14 00:33:23 +0530

    To do added

--- App.js
+++ App.js
@@ -1,18 +1,48 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text,View,SafeAreaView} from 'react-native';
+import React, {Component,useState} from 'react';
+import {StyleSheet, Text,TextInput,Button,View,SafeAreaView, ToolbarAndroidBase} from 'react-native';
+import Todo from './Todo';
+
 
 const App = () =>{
-  return(
+  const [input,setInput] =useState('');
+
+  const [todos,setTodos] =useState([]);
+  
+  
+  const addTodo = () =>{
+  setTodos([input, ...todos]);
+  setInput(''); 
+  }
+
+
+
+  return (
     <SafeAreaView>
-      <View >
-        <Text style={styles.firstLabel}>Hello Worlds</Text>
+      <View>
+        <Text style={styles.firstLabel}>To do react native</Text>
       </View>
+      {todos.map(todo=>(
+        <Todo  title={todo}/>
+      ))}
+
+
+      <TextInput
+        style={styles.todoInput}
+        value={input}
+        onChangeText={(text) => setInput(text)}
+      />
+      <Button title='Add TODO' onPress={addTodo}/>
     </SafeAreaView>
-    
   );
 }
 
 const styles = StyleSheet.create({
+todoInput:{
+margin:20,
+height:40,
+borderColor:'red',
+borderWidth:1
+},
   firstLabel:{
 backgroundColor: 'red',
 fontSize : 30
--- Todo.js
+++ Todo.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-function Todo() {
+const Todo = ({title} ) =>{
     return (
-        <div>
-            
-        </div>
+        <View>
+            <Text>{title}</Text>
+        </View>
     )
 }
 
 export default Todo
+
+const styles = StyleSheet.create({})
+
