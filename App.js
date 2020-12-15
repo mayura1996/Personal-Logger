3e2e08f989d87476577dcc5086da46b5294f5366
mayura1996 <mmayurapavan@gmail.com>
2020-12-15 23:37:21 +0530

    Done

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
-import React, {Component,useState} from 'react';
-import {StyleSheet, Text,TextInput,Button,View,SafeAreaView, ToolbarAndroidBase} from 'react-native';
+import React, {Component,useState,useEffect} from 'react';
+import {StyleSheet, Text,ScrollView,TextInput,Button,View,SafeAreaView, ToolbarAndroidBase} from 'react-native';
 import Todo from './Todo';
-
+import {
+  BarChart
+} from "react-native-chart-kit";
 
 const App = () =>{
-  const [input,setInput] =useState('');
+  const [description,setDescription] =useState('');
+  const [amount,setAmount]=useState('');
+  const [total,setTotal] =useState(0)
+  const [gigs,setGigs] =useState([
+    {
+      description : 'Freelance job with Mayura',
+      amount : 499.99
+    }
+  ]);
+  
+  useEffect(() => {
+  setTotal(gigs.reduce((total, gig) => total+Number(gig.amount),0));
+  }, [gigs])
 
-  const [todos,setTodos] =useState([]);
-  
-  
-  const addTodo = () =>{
-  setTodos([input, ...todos]);
-  setInput(''); 
+
+
+  const addGig = () =>{
+setGigs([...gigs,{
+  description: description,
+  amount: amount
+}])
+
+setDescription('');
+setAmount('');
   }
 
 
@@ -20,24 +38,40 @@ const App = () =>{
     <SafeAreaView>
       <View>
         <Text style={styles.firstLabel}>To do react native</Text>
+      <Text>{total}</Text>
       </View>
-      {todos.map(todo=>(
-        <Todo  title={todo}/>
-      ))}
-
-
+ 
       <TextInput
         style={styles.todoInput}
-        value={input}
-        onChangeText={(text) => setInput(text)}
+        value={description}
+        placeholder='Enter a description'
+        onChangeText={(text) => setDescription(text)}
       />
-      <Button title='Add TODO' onPress={addTodo}/>
+     <TextInput
+        style={styles.todoInput}
+        value={amount}
+        keyboardType='numeric'
+        placeholder='Enter the amount you made in USD ($)'
+        onChangeText={(text) => setAmount(text)}
+      />
+
+      <Button disabled={!amount || !description } title='Add Gig' onPress={addGig}/>
+    {
+      gigs.map(gig =>(
+        <View>
+        <Text>{gig.description}</Text>
+        <Text>{gig.amount}</Text>
+        </View>
+      ))
+    }
+    
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
 todoInput:{
+  marginBottom:10,
 margin:20,
 height:40,
 borderColor:'red',
